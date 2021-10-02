65fdf2bfe39824af93ee93e138f2e992368e017f
Vinnie6167 <vinnietombari@gmail.com>
2021-10-01 23:13:19 -0400

    djano rest_framework added

--- GoLocal/GoLocal/settings.py
+++ GoLocal/GoLocal/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api.apps.ApiConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- GoLocal/api/views.py
+++ GoLocal/api/views.py
@@ -1,5 +1,8 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view(['GET'])
 def getRoutes(request):
   routes = [
     {
@@ -33,4 +36,4 @@ def getRoutes(request):
       'description': 'Creates a new store'
     },
   ]
-  return JsonResponse(routes, safe=False)
+  return Response(routes)
