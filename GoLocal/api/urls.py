48d04bfedbf3278aef7662889d8997a32d315f02
Sam Sampey <ssampey@andrew.cmu.edu>
2021-10-02 02:01:58 -0400

    Delete Bug Fix

--- GoLocal/api/urls.py
+++ GoLocal/api/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
   path('', views.getRoutes),
   path('stores/', views.getStores),
   path('stores/create/', views.createStore),
+  path('stores/<str:pk>/delete/', views.deleteStore),
   path('stores/<str:pk>/', views.getStore),
   path('products/', views.getProducts),
   path('products/create/', views.createProduct),
+  path('products/<str:pk>/delete/', views.deleteProduct),
   path('products/<str:pk>/', views.getProduct),
 ]
