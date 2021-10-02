9e0c4673c9683432aaee7e0ceaa6ef7197c0bea9
Sam Sampey <ssampey@andrew.cmu.edu>
2021-10-01 23:50:07 -0400

    Fixed Bugs

--- GoLocal/api/views.py
+++ GoLocal/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import StoreSerializer, ProductSerializer
+from .serializerStore import StoreSerializer
+from .serializerProduct import ProductSerializer
 from .models import Store, Product 
 
 @api_view(['GET'])
