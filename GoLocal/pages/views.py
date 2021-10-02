8a83676d9c365885358cb23acdf10cb6f5417420
Vinnie6167 <vinnietombari@gmail.com>
2021-10-02 09:37:32 -0400

    Hardcode view - temporary

--- GoLocal/pages/views.py
+++ GoLocal/pages/views.py
@@ -3,10 +3,10 @@ import requests
 
 # Create your views here.
 def mainPage(request):
-  resp = requests.get(url="http://127.0.0.1:8000/stores")
+  resp = '[{"id":1,"name":"Giant Eagle","location":"123 Main St, Pittsburgh, Pennsylvania","phone":"1234567890","website":"gianteagle.com","hoursOfOperation":10},{"id":3,"name":"Target","location":"123 Main St, Pittsburgh, Pennsylvania","phone":"1234567890","website":"gianteagle.com","hoursOfOperation":10}]'
   store_list = resp.json()
 
-  resp = requests.get(url="http://127.0.0.1:8000/products")
+  resp = '[{"id":1,"name":"bread","price":5,"store":[1]},{"id":2,"name":"bread","price":5,"store":[1]}]'
   product_list = resp.json()
 
   return render(request, "index.html", {"store_list": store_list, "product_list": product_list})
