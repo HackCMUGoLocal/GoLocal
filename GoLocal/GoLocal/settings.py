cd266b247d6248a79d657c306444e39ffb209f63
prabhar2042 <33375822+prabhar2042@users.noreply.github.com>
2021-10-02 10:45:28 -0400

    more html

--- GoLocal/GoLocal/settings.py
+++ GoLocal/GoLocal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z$5x*pvcuxm^ju9c^4bw8@#)bw0u6arcu9li=l)tthi)e161v2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vinnie6167.pythonanywhere.com']
+ALLOWED_HOSTS = ['vinnie6167.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
